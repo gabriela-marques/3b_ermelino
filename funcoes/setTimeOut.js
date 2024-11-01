1f992d3b72609fe4f044041c7aed7f16d3978ffa
gabriela-marques <gabriela.sutil.santos@escola.pr.gov.br>
2024-11-01 18:20:03 +0000

    clear interval e setinterval do javascript

--- funcoes/setTimeOut.js
+++ funcoes/setTimeOut.js
@@ -1,4 +1,18 @@
 // setTimeOut(função, tempo ms)
 setTimeout(function(){
     console.log("tempo esgotado")
-}, 7000)
+}, 1000)
+
+// setInterval(função, tempo ms)
+let fps = Math.round ( Math.random() *30)
+
+const intervalo1 = setInterval(function() {
+    console.clear() // limpar a tela
+    console.log("fps: " + fps)
+    fps = fps + 1
+}, 1000)
+
+setTimeout(function (){
+    clearInterval(intervalo1)
+    console.log("conexão encerrada")
+}, 5000)
